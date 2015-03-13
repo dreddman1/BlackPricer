b08397acc8d3c12278291d69bbb008201f69140f
dreddman1 <dreddman1@hotmail.com>
2015-03-13 17:07:34 +0000

    Small changes to data.

--- src/Pricer/com/Pricer/BlackPricer.java
+++ src/Pricer/com/Pricer/BlackPricer.java
@@ -9,9 +9,9 @@ public class BlackPricer {
     private static final double STRIKE = 100D;
     private static final OptionKind OPTION_KIND = OptionKind.CALL;
     private static final double FORWARD = 110D;
-    private static final double VOLATILITY = 100;
+    private static final double VOLATILITY = 0.16;
     private static final double VOLA_TIME_TO_EXPIRY = 0.5;
-    private static final double INTEREST_RATE = 10;
+    private static final double INTEREST_RATE = 0.01;
 
     public static void main(String[] args) {
         BlackPriceCalculator myBlackPriceCalculator = new BlackPriceCalculatorFactory().create();
